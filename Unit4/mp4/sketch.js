7270806451cffabbc28b0afef93d3cac960dd0a4
harrison gordon <hgordon@ilstu.edu>
2022-04-28 05:44:11 -0500

    taco part 1

--- Unit4/mp4/sketch.js
+++ Unit4/mp4/sketch.js
@@ -1,7 +1,22 @@
+// must be in HTTPS
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(windowWidth, windowHeight);
+  console.log('starting');
+	noStroke();
+  // get position once
+  if (!navigator.geolocation) {
+    alert("navigator.geolocation is not available");
+  }
+  navigator.geolocation.getCurrentPosition(setPos);
 }
 
-function draw() {
-
+function setPos(position) {
+  var lat = position.coords.latitude;
+  var lng = position.coords.longitude;
+  background(0);
+  fill(255);
+  textSize(32);
+  text("Current posish: " + nf(lat,2,2) + " " + nf(lng,2,2), 10, height/2);
+  textSize(32);
+  text("taco bell " + 40.51 + " " + 88.95, 125, 50);
 }
