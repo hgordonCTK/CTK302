665c6de3fea9ff7961d03f2f9c1faf605efc48af
harrison gordon <hgordon@ilstu.edu>
2022-04-28 08:13:04 -0500

    mp4final

---
+++ mp4/sketch.js
@@ -0,0 +1,26 @@
+// must be in HTTPS
+
+  
+ 
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  console.log('starting');
+  noStroke();
+  // get position once
+  if (!navigator.geolocation) {
+    alert("navigator.geolocation is not available");
+  }
+  navigator.geolocation.getCurrentPosition(setPos);
+}
+
+function setPos(position) {
+  var lat = position.coords.latitude;
+  var lng = position.coords.longitude;
+ 
+fill(255);
+  textSize(32);
+  text("Current posish: " + nf(lat, 2, 2) + " " + nf(lng, 2, 2), 10, height / 2);
+  textSize(32);
+  text("taco bell " + 40.51 + " " + 88.95, 125, 50);
+
+}
